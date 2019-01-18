09270f5cfca536881560c58aa8536077f9ae2f56
Milorad Trninic <milbor16@gmail.com>
2019-01-18 02:56:41 +0100

    Ethereum stream configured

--- spark-stream/producer/producer.py
+++ spark-stream/producer/producer.py
@@ -2,7 +2,7 @@ from kafka import KafkaProducer
 import kafka.errors
 import json
 import websocket
-from web3 import Web3
+from web3.auto import Web3
 from web3 import providers
 import os
 import time
@@ -16,6 +16,7 @@ WS_SERVER = 'wss://kovan.infura.io/ws'
 SUB_MSG = '{"jsonrpc":"2.0", "id": 1, "method": "eth_subscribe", "params": ["newPendingTransactions"]}'
 TOPIC = 'ethereum'
 
+
 while True:
     try:
         #producer = KafkaProducer(bootstrap_servers=os.environ['KAFKA_HOST'])
@@ -26,31 +27,27 @@ while True:
         time.sleep(3)
 
 def on_message(ws, message):
+
     tx = json.loads(message)
-    print(auto.w3.eth.getTransaction(tx['params']['result']))
-    #producer.send(TOPIC, value=json.dumps()
-
-
-def on_error(ws, error):
-    print(error)
-
-
-def on_close(ws):
-    print("### closed ###")
-
-
-def on_open(ws):
-    def run(*args):
-        ws.send(SUB_MSG)
-    thread.start_new_thread(run, ())
+    print(tx['params']['result'])
+    print(web3.eth.getTransaction(tx['params']['result']))
+    #producer.send(TOPIC, value=json.dumps(tx[u'x']), key=str(tx[u'x'][u'hash']))
+    # print(message)
 
 
 if __name__ == "__main__":
+
     web3 = Web3(providers.WebsocketProvider("wss://mainnet.infura.io/_ws/"))
     web3_pending_filter = web3.eth.filter('pending')
-
     while True:
-        transaction_list = web3.eth.getFilterChanges(web3_pending_filter.filter_id)
+        transaction_list = web3_pending_filter.get_new_entries()
+        transactions = [(web3.eth.getTransaction(h)) for h in transaction_list if h is not None]
+        [print(tx) for tx in transactions if tx is not None]
 
-        transactions = [web3.eth.getTransaction(h) for h in transaction_list]
-        print(transactions)
+    # ws = websocket.WebSocketApp(WS_SERVER,
+    #                             on_message=on_message,
+    #                             on_error=on_error,
+    #                             on_close=on_close)
+    # ws.on_open = on_open
+    # ws.run_forever()
+    # websocket.enableTrace(True)
